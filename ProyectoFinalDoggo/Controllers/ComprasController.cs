f9de1b1ae9a65053b0ab2d1bfe0337f19aaae2ec
YwYcr <alvaradoocr@gmail.com>
2023-03-18 16:07:27 -0600

    Commit clase compras

---
+++ ProyectoFinalDoggo/Controllers/ComprasController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalDoggo.Models;
+using ProyectoFinalDoggo.clases;
+
+namespace ProyectoFinalDoggo.Controllers
+{
+    public class ComprasController : Controller
+    {
+        Compra compra = new Compra();
+        // GET: Compras
+        public ActionResult Index()
+        {
+
+            IEnumerable<compras> lst = compra.Consultar();
+
+            return View(lst);
+
+        }
+
+
+        public ActionResult Eliminar(int id)
+        {
+            compras modelo = new compras()
+            {
+                IDTrans = id
+            };
+            compra.Eliminar(modelo);
+            ViewBag.valor = " La Compra fue eliminada ";
+            IEnumerable<compras> lst = compra.Consultar();
+
+            return View("Index", lst);
+        }
+
+        public ActionResult Guardar(compras modelo)
+        {
+            ViewBag.valor = " ";
+            return View(modelo);
+        }
+        public ActionResult Nuevo(compras modelo)
+        {
+            compra.Guardar(modelo);
+            ViewBag.mensaje = "Se guardo Correctamente";
+            return View("Guardar", modelo);
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            compras modelo = compra.Consulta(id);
+            ViewBag.valor = " ";
+            return View(modelo);
+
+        }
+
+        public ActionResult Cambiar(compras modelo)
+        {
+            compra.Modificar(modelo);
+            ViewBag.valor = " ";
+            return View("Modificar", modelo);
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            compras modelo = compra.Consulta(id);
+            return View(modelo);
+
+        }
+    }
+}
---
+++ ProyectoFinalDoggo/clases/Compra.cs
@@ -0,0 +1,58 @@
+﻿using ProyectoFinalDoggo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalDoggo.clases
+{
+    public class Compra
+    {
+        public IEnumerable<compras> Consultar()
+        {
+            using (g5_ProyectoFinalDoggoEntities2 db = new g5_ProyectoFinalDoggoEntities2())
+            {
+                return db.compras.ToList();
+            }
+        }
+        public void Eliminar(compras modelo)
+        {
+            using (g5_ProyectoFinalDoggoEntities2 db = new g5_ProyectoFinalDoggoEntities2())
+            {
+                db.Entry(modelo).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+
+            }
+
+        }
+        public void Guardar(compras modelo)
+        {
+            using (g5_ProyectoFinalDoggoEntities2 db = new g5_ProyectoFinalDoggoEntities2())
+            {
+                db.compras.Add(modelo);
+                db.SaveChanges();
+            }
+
+        }
+
+        public void Modificar(compras modelo)
+        {
+            using (g5_ProyectoFinalDoggoEntities2 db = new g5_ProyectoFinalDoggoEntities2())
+            {
+                db.Entry(modelo).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+
+        }
+
+        public compras Consulta(int id)
+        {
+            using (g5_ProyectoFinalDoggoEntities2 db = new g5_ProyectoFinalDoggoEntities2())
+            {
+                return db.compras.FirstOrDefault(x => x.IDTrans == id);
+            }
+
+        }
+    }
+}
