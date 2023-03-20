839ee26f66cbe604553ea26e6cf7e8a47c68474f
AnaBM97 <110143155+AnaBM97@users.noreply.github.com>
2023-03-19 20:14:46 -0600

    Update Usuarios.cs

--- ProyectoFinalDoggo/Models/Usuarios.cs
+++ ProyectoFinalDoggo/Models/Usuarios.cs
@@ -11,6 +11,8 @@ namespace ProyectoFinalDoggo.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Usuarios
     {
@@ -21,13 +23,17 @@ namespace ProyectoFinalDoggo.Models
         }
     
         public string usuario { get; set; }
+        [Required(ErrorMessage = "Dato requerido")]
         public string pass { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Dato requerido")]
         public string correo { get; set; }
         public string direccion { get; set; }
         public string telefono { get; set; }
         public string rol { get; set; }
         public string nombre { get; set; }
         public string apellido { get; set; }
+        public string LoginErrorMessage { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<compras> compras { get; set; }
