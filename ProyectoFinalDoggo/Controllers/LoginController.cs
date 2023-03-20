f84a8734dc5fb9689a1a7bc38ba4ab984cf38cf9
AnaBM97 <110143155+AnaBM97@users.noreply.github.com>
2023-03-19 20:07:26 -0600

    Add files via upload

---
+++ ProyectoFinalDoggo/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalDoggo.Models;
+
+namespace ProyectoFinalDoggo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(ProyectoFinalDoggo.Models.Usuarios userModel)
+        {
+            using (g5_ProyectoFinalDoggoEntities2 db = new g5_ProyectoFinalDoggoEntities2())
+            {
+                var userDetails = db.Usuarios.Where(x => x.usuario == userModel.usuario && x.pass == userModel.pass).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Contraseña o Usuario incorrectos";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["Usuario"] = userDetails.usuario;
+                    return RedirectToAction("Back to List", "Index");
+                }
+            }
+        }
+        public ActionResult LogOut()
+        {
+            int Usuario = (int)Session["Usuario"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
