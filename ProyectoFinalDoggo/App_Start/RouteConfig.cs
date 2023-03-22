da486364669f86a6c629627007027b2000987c5d
sebaslizano7194 <slizano40347@ufide.ac.cr>
2023-03-21 18:19:15 -0600

    Update RouteConfig.cs

--- ProyectoFinalDoggo/App_Start/RouteConfig.cs
+++ ProyectoFinalDoggo/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProyectoFinalDoggo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Productos", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
