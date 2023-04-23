9f5b5aca12dccf0b57ab6b479f3cefea43205bae
AnaBM97 <110143155+AnaBM97@users.noreply.github.com>
2023-04-22 19:43:55 -0600

    Add files via upload

---
+++ ProyectoFinalDoggo/Filtros/VerificarProductos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalDoggo.Controllers;
+
+namespace ProyectoFinalDoggo.Filtros
+{
+    public class VerificarProductos : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.Controller is ProductosController == false)
+            {
+
+                filterContext.HttpContext.Response.Redirect("~/Productos/Index");
+            }
+        }
+
+    }
+}
